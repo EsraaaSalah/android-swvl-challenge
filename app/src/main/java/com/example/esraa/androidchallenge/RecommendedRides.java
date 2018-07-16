2d14d5a30c316872252b6677afe5de75761f7d4e
EsraaaSalah <esraasalah171995@gmail.com>
2018-07-16 16:31:10 +0200

    Project Setup

---
+++ app/src/main/java/com/example/esraa/androidchallenge/RecommendedRides.java
@@ -0,0 +1,13 @@
+package com.example.esraa.androidchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendedRides extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended_rides);
+    }
+}
