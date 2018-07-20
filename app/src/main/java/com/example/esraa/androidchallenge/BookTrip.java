1d488649343c87ef5a884e4c94eaead38985be23
EsraaaSalah <esraasalah171995@gmail.com>
2018-07-20 04:47:02 +0200

    Book Button Activated

--- app/src/main/java/com/example/esraa/androidchallenge/BookTrip.java
+++ app/src/main/java/com/example/esraa/androidchallenge/BookTrip.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BookTrip extends AppCompatActivity {
     private String rideId;
@@ -24,6 +26,13 @@ public class BookTrip extends AppCompatActivity {
         dropOffId = intent.getStringExtra("dropOffId");
         pickUpCoordinates = intent.getDoubleArrayExtra("pickUpCoordinates");
         dropOffCoordinates = intent.getDoubleArrayExtra("dropOffCoordinates");
+
+        Button buttonBook = (Button) findViewById(R.id.buttonBook);
+        buttonBook.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(BookTrip.this,"Booking",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
