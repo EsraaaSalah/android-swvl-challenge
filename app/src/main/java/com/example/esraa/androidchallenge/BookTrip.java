66a9410df34967a1ab43f00b5ee1dae5f34639f6
EsraaaSalah <esraasalah171995@gmail.com>
2018-07-22 18:35:21 +0200

    Booking trip feature activated

--- app/src/main/java/com/example/esraa/androidchallenge/BookTrip.java
+++ app/src/main/java/com/example/esraa/androidchallenge/BookTrip.java
@@ -1,11 +1,8 @@
 package com.example.esraa.androidchallenge;
 
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,7 +13,20 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookTrip extends AppCompatActivity {
     private String rideId;
@@ -29,8 +39,10 @@ public class BookTrip extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_trip);
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
+
         rideId = bundle.getString("rideId");
         pickUpId = bundle.getString("pickUpId");
         dropOffId = bundle.getString("dropOffId");
@@ -40,14 +52,12 @@ public class BookTrip extends AppCompatActivity {
         Button buttonBook = (Button) findViewById(R.id.buttonBook);
         buttonBook.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(BookTrip.this,"Booking",Toast.LENGTH_SHORT).show();
+                sendPostRequest();
             }
         });
 
-        Log.e("Pickup", coordinatesToString(pickUpCoordinates));
-        Log.e("DropOff", coordinatesToString(dropOffCoordinates));
-
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
         url = "https://maps.googleapis.com/maps/api/staticmap?" +
                 "&size=360x607&maptype=roadmap&scale=2" +
                 "&markers=color:red%7C" + coordinatesToString(pickUpCoordinates)+
@@ -60,5 +70,53 @@ public class BookTrip extends AppCompatActivity {
     public static String coordinatesToString(Object[] arr)
     {
         return arr[0] + "," + arr[1];
+    }
+    public void sendPostRequest()
+    {
+        String url = "http:" +
+                "//private-fc685a-swvlandroidjuniorchallenge.apiary-mock.com/book";
+        RequestQueue queue = Volley.newRequestQueue(BookTrip.this);
+        Response.Listener<String> responseListener = new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response) {
+                Log.e("Response","Successfully booked");
+                Toast.makeText(BookTrip.this,"Your Trip is successfully booked",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        Response.ErrorListener errorListener = new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error.Response", error.toString());
+                Toast.makeText(BookTrip.this,"An Error Occurred",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                responseListener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                JSONObject body = new JSONObject();
+                try {
+                    body.put("ride_id", rideId);
+                    body.put("pickup_id", pickUpId);
+                    body.put("dropoff_id", dropOffId);
+                } catch (JSONException e) {
+                    Log.e("JSON Exception", e.toString());
+                    Toast.makeText(BookTrip.this,"An Error Occurred",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return body.toString().getBytes();
+            }
+        };
+        queue.add(postRequest);
     }
 }
